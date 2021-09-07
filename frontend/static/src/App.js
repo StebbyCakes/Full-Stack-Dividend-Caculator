57f10a0f6a555ef5216981e310d31163d0fc8726
Esteban Carter <carterea1999@gmail.com>
2021-09-07 16:11:23 -0400

    Initial commit

---
+++ frontend/static/src/App.js
@@ -0,0 +1,22 @@
+import { Component } from 'react';
+import './App.css';
+
+class App extends Component{
+  constructor(props) {
+    super(props);
+      this.state={
+
+      }
+
+      // binding stuff
+  }
+  render(){
+    return(
+      <div classname = "container">
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
+
+export default(App);
